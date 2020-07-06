419eb510dc49e4261a1a2e8a16560171fc1ac282
Abhihshek Pal <palabhishek322@gmail.com>
2020-07-06 14:03:17 +0530

    Add project files.

---
+++ DataAccessLayer/DbContext/LSDbContext.cs
@@ -0,0 +1,19 @@
+﻿//using Microsoft.AspNet.Identity.EntityFramework;
+using LastStudy.Core.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DbContext
+{
+    public class LSDbContext : IdentityDbContext<LSUser>
+    {
+        public LSDbContext() : base("LSBaseDb")
+        {
+
+        }
+    }
+}
